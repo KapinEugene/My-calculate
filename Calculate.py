60dbe47ffb56f00efffcb3f7510fdd568ea940c3
KapinEugene <kapin020797@gmail.com>
2022-04-11 21:58:36 +0500

    Test exam 1

--- Calculate.py
+++ Calculate.py
@@ -1,23 +1,23 @@
 class Calculate():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, first_number, second_number):
+        self.first_number = first_number
+        self.second_number = second_number
 
     # def __str__(self):
     #     return f'Результат операции {self.rez}'
 
     def addition(self):
-        rez = self.x + self.y
+        rez = self.first_number + self.second_number
         return rez
 
     def substraction(self):
-        rez = self.x - self.y
+        rez = self.first_number - self.second_number
         return rez
 
     def multiplication(self):
-        rez = self.x * self.y
+        rez = self.first_number * self.second_number
         return rez
 
     def division(self):
-        rez = self.x / self.y
+        rez = self.first_number / self.second_number
         return rez
--- main.py
+++ main.py
@@ -1,59 +1,76 @@
 from Calculate import Calculate
 import roman
+import re
 
 
 while(True):
-    a = 0
-    x = input("Введите первое число ")
-    y = input("Введите второе число ")
-    if x.isdigit() != True and y.isdigit() != True:
-        x = roman.fromRoman(x.upper())
-        y = roman.fromRoman(y.upper())
-        a = 1
-    elif x.isdigit() != True and y.isdigit() == True:
-        x = roman.fromRoman(x.upper())
-        y = int(y)
-        a = 1
-        print('Нельзя производить операцию с арабским и римским число, но если вы настаиваете, то')
-    elif x.isdigit() == True and y.isdigit() != True:
-        y = roman.fromRoman(y.upper())
-        x = int(x)
-        a = 1
-        print('Нельзя производить операцию с арабским и римским число, но если вы настаиваете, то')
-    elif x.isdigit() == True and y.isdigit() == True:
-        x = int(x)
-        y = int(y)
-    test = Calculate(x, y)
-    o = input("Введите операцию: ")
-    if o == '+':
-        if a == 0:
+    is_arabic = True
+    expression = input('Введите выражение: ')
+    if '+' in expression:
+        expression = expression.replace(' ', '')
+        expression = expression.split('+')
+        first_number = expression[0]
+        second_number = expression[1]
+        operation = '+'
+    elif '-' in expression:
+        expression = expression.replace(' ', '')
+        expression = expression.split('-')
+        first_number = expression[0]
+        second_number = expression[1]
+        operation = '-'
+    elif '*' in expression:
+        expression = expression.replace(' ', '')
+        expression = expression.split('*')
+        first_number = expression[0]
+        second_number = expression[1]
+        operation = '*'
+    elif '/' in expression:
+        expression = expression.replace(' ', '')
+        expression = expression.split('/')
+        first_number = expression[0]
+        second_number = expression[1]
+        operation = '/'
+    if (first_number.isdigit() and not second_number.isdigit()) or \
+            (not first_number.isdigit() and second_number.isdigit()):
+        print('Нельзя производить операцию с арабским и римским числом')
+        continue
+    elif not first_number.isdigit() and not second_number.isdigit():
+        first_number = roman.fromRoman(first_number.upper())
+        second_number = roman.fromRoman(second_number.upper())
+        is_arabic = False
+    elif first_number.isdigit() and second_number.isdigit():
+        first_number = int(first_number)
+        second_number = int(second_number)
+    test = Calculate(first_number, second_number)
+    if operation == '+':
+        if is_arabic:
             print(test.addition())
-        elif a == 1:
+        elif not is_arabic:
             print(roman.toRoman(test.addition()))
-    elif o == '-':
-        if a == 0:
+    elif operation == '-':
+        if is_arabic:
             print(test.substraction())
-        elif a == 1:
+        elif not is_arabic:
             print(roman.toRoman(test.substraction()))
-    elif o == '*':
-        if a == 0:
+    elif operation == '*':
+        if is_arabic:
             print(test.multiplication())
-        elif a == 1:
+        elif not is_arabic:
             print(roman.toRoman(test.multiplication()))
-    elif o == '/':
-        if a == 0:
-            if y == 0:
+    elif operation == '/':
+        if is_arabic:
+            if second_number == 0:
                 try:
                     test.division()
                 except ZeroDivisionError as e:
                     print('На ноль делить нельзя, попробуйте еще раз')
             else:
                 print(test.division())
-        elif a == 1:
-            if y == 0:
+        elif not is_arabic:
+            if second_number == 0:
                 try:
                     test.division()
                 except ZeroDivisionError as e:
                     print('На ноль делить нельзя, попробуйте еще раз')
             else:
-                print(roman.toRoman(test.division()))
+                print(roman.toRoman(int(test.division())))
